d93081cbff6222da36623c94270b0b7cdbf4e7bf
cg-yamada <cg-yamada@users.noreply.github.com>
2023-12-21 13:17:44 +0900

    fix:

--- src/script.js
+++ src/script.js
@@ -1,15 +1,32 @@
 function getAllLinkElements(elementId) {
+  console.log("クリック");
   const container = document.getElementById(elementId);
   const allLinks = container.querySelectorAll("a");
 
-  return Array.from(allLinks)
-    .map((el) => {
+  const links = Array.from(allLinks)
+    .forEach((el) => {
       const href = el.getAttribute("href");
       return href ? { href, children: el.textContent.trim() } : null;
     })
     .filter(Boolean);
+
+  const linkListContainer = document.getElementById("js-links");
+  links.forEach((link) => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `<a href="${link.href}">${link.children}</a>`;
+    linkListContainer.appendChild(listItem);
+  });
+
+  return links;
 }
 
-window.onload = () => {
-  console.log(getAllLinkElements("__next"));
-};
+// window.onload = () => {
+//   const links = getAllLinkElements("__next");
+
+//   const linkListContainer = document.getElementById("js-links");
+//   links.map((link) => {
+//     const listItem = document.createElement("li");
+//     listItem.innerHTML = `<a href="${link.href}">${link.children}</a>`;
+//     linkListContainer.appendChild(listItem);
+//   });
+// };
